2fb7d9c21883fb579a61f239f6d4cad5b90d255b
helenavprado <helena.vprado@hotmail.com>
2022-11-27 10:21:10 -0800

    last commit before switching branch

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./components/App/App";
 import store from "./components/App/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+let variavel = root.render(
   <React.StrictMode>
     {/* next step: figure out how to send info as PROPS */}
     <App dispatch={store.dispatch} />
